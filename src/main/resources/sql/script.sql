a842f25bc521a4545935e9719e798362f7d600a5
Martin Vu <22mvu7@gmail.com>
2025-10-11 04:14:01 -0700

    modified script and added readme file

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -1,6 +1,8 @@
 -- /usr/local/mysql/bin/mysql -u mytestuser -p < /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/script.sql
 USE moviedb;
 
+SET GLOBAL autocommit = 0;
+
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/createtable.sql;
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/credit_cards.sql;
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/customers.sql;
@@ -11,3 +13,6 @@ SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/sales.sql
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/stars.sql;
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/genres_in_movies.sql;
 SOURCE /Users/martinvu/2025-fall-cs-122b-marjoe/src/main/resources/sql/stars_in_movies.sql;
+
+COMMIT;
+COMMIT; SET GLOBAL autocommit = 1;
